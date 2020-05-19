83bac821348f007435090b8195a60b648e63f130
AlterMann <MoskaletsAndrey@yandex.ru>
2020-05-19 13:53:29 +0300

    сконфигурировал с помощью аннотаций и java-конфигурации

--- network-chat/src/main/java/ru/geekbrains/server/ChatServerApplication.java
+++ network-chat/src/main/java/ru/geekbrains/server/ChatServerApplication.java
@@ -1,13 +1,15 @@
 package ru.geekbrains.server;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.sql.SQLException;
 
 public class ChatServerApplication {
     public static void main(String[] args) throws SQLException {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringContext.class);
         ChatServer chatServer = context.getBean("chatServer", ChatServer.class);
 
         chatServer.start(7777);
