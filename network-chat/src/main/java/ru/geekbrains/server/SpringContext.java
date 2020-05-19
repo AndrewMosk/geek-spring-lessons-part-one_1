59a8fa7fd6a07f50a55141c2885f4a8ad8e7c1e4
AlterMann <MoskaletsAndrey@yandex.ru>
2020-05-19 16:04:23 +0300

    пост процессоры

--- network-chat/src/main/java/ru/geekbrains/server/SpringContext.java
+++ network-chat/src/main/java/ru/geekbrains/server/SpringContext.java
@@ -1,6 +1,7 @@
 package ru.geekbrains.server;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import ru.geekbrains.server.auth.AuthService;
@@ -11,6 +12,7 @@ import javax.sql.DataSource;
 import java.sql.SQLException;
 
 @Configuration
+@ComponentScan(basePackages = "ru.geekbrains.server.processors")
 public class SpringContext {
 
     @Bean
