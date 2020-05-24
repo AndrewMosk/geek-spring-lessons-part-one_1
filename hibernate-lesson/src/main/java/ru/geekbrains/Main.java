9ed7e993dad308928442cc25456c1a5968cc037f
AlterMann <MoskaletsAndrey@yandex.ru>
2020-05-24 21:47:39 +0300

    select + update

--- hibernate-lesson/src/main/java/ru/geekbrains/Main.java
+++ hibernate-lesson/src/main/java/ru/geekbrains/Main.java
@@ -14,11 +14,25 @@ public class Main {
                 .buildSessionFactory();
 
         // INSERT User
+//        EntityManager em = emFactory.createEntityManager();
+//        User user = new User(null, "will", "pass");
+//
+//        em.getTransaction().begin();
+//        em.persist(user);
+//        em.getTransaction().commit();
+
+        // SELECT User
+//        EntityManager em = emFactory.createEntityManager();
+//        User user = em.find(User.class, 1L);
+//        System.out.println(user);
+
+        // UPDATE User
         EntityManager em = emFactory.createEntityManager();
-        User user = new User(null, "alex", "password");
+        User user = em.find(User.class, 1L);
+        System.out.println(user);
 
         em.getTransaction().begin();
-        em.persist(user);
+        user.setName("Alexey");
         em.getTransaction().commit();
     }
 }
