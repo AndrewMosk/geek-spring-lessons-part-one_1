6a08ad209b11934ce1a8605ce96344108396cf40
AlterMann <MoskaletsAndrey@yandex.ru>
2020-06-03 13:27:08 +0300

    изменение/удаление пользователя

--- spring-boot-lesson/src/main/java/ru/geekbrains/service/UserService.java
+++ spring-boot-lesson/src/main/java/ru/geekbrains/service/UserService.java
@@ -67,4 +67,8 @@ public class UserService {
     public Optional<User> findById(long id) {
         return repository.findById(id);
     }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
 }
