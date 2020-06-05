794a1e75319096a2980acbd625b9665ce642cd1d
AlterMann <MoskaletsAndrey@yandex.ru>
2020-06-05 22:22:57 +0300

    добавил REST API пользователю и товару

--- spring-boot-lesson/src/main/java/ru/geekbrains/persist/entity/User.java
+++ spring-boot-lesson/src/main/java/ru/geekbrains/persist/entity/User.java
@@ -1,5 +1,7 @@
 package ru.geekbrains.persist.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -20,6 +22,7 @@ public class User {
     @Column(length = 32)
     private String password;
 
+    @JsonIgnore
     @Transient // не сохраняется в бд
     private String repeatPassword;
 
